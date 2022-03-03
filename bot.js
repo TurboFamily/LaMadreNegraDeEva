c43afadcb819f10251932c2b123fcb240611dd99
Dekotale <33232965+dekotale@users.noreply.github.com>
2022-03-03 22:05:27 +0100

    asdas

--- bot.js
+++ bot.js
@@ -65,7 +65,8 @@ function playAudio() {
 }
 
 function sigueYEructa(canalID){
-    voiceChannel = bot.channels.cache.get(canalID);
+    config.voiceChannel = canalID;
+    voiceChannel = bot.channels.cache.get(config.voiceChannel);
     if (!voiceChannel) return console.error('The voice channel does not exist!\n(Have you looked at your configuration?)');
 
     voiceChannel.join().then(connection => {
