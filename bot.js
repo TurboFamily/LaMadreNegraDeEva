40fd8ae0dff4aded66902859149be4d64dde2ece
Dekotale <33232965+dekotale@users.noreply.github.com>
2022-03-03 21:54:31 +0100

    xdd tos los canales

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 /**************************************************************************
- * 
+ *
  *  DLMP3 Bot: A Discord bot that plays local mp3 audio tracks.
  *  (C) Copyright 2020
- *  Programmed by Andrew Lee 
- *  
+ *  Programmed by Andrew Lee
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -16,7 +16,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  ***************************************************************************/
 const Discord = require('discord.js');
 const fs = require('fs');
@@ -30,162 +30,196 @@ let fileData;
 bot.login(config.token);
 
 function playAudio() {
-  voiceChannel = bot.channels.cache.get(config.voiceChannel);
-  if (!voiceChannel) return console.error('The voice channel does not exist!\n(Have you looked at your configuration?)');
-  
-  voiceChannel.join().then(connection => {
-    let files = fs.readdirSync('./music');
+    voiceChannel = bot.channels.cache.get(config.voiceChannel);
+    if (!voiceChannel) return console.error('The voice channel does not exist!\n(Have you looked at your configuration?)');
 
-    while (true) {
-      audio = files[Math.floor(Math.random() * files.length)];
-      console.log('Searching .mp3 file...');
-      if (audio.endsWith('.mp3')) {
-        break;
-      }
-    }
+    voiceChannel.join().then(connection => {
+        let files = fs.readdirSync('./music');
 
-    dispatcher = connection.play('./music/' + audio);
-    
-    dispatcher.on('start', () => {
+        while (true) {
+            audio = files[Math.floor(Math.random() * files.length)];
+            console.log('Searching .mp3 file...');
+            if (audio.endsWith('.mp3')) {
+                break;
+            }
+        }
+
+        dispatcher = connection.play('./music/' + audio);
+
+        dispatcher.on('start', () => {
+
+        });
+
+        dispatcher.on('error', console.error);
+
+        dispatcher.on('finish', () => {
+            voiceChannel = bot.channels.cache.get(config.voiceChannel);
+            dispatcher.destroy();
+            voiceChannel.leave();
+        });
+
+    }).catch(e => {
+        console.error(e);
+    });
+
+}
+
+function sigueYEructa(){
+    voiceChannel = bot.channels.cache.get(config.voiceChannel);
+    if (!voiceChannel) return console.error('The voice channel does not exist!\n(Have you looked at your configuration?)');
+
+    voiceChannel.join().then(connection => {
+        let files = fs.readdirSync('./music');
+
+        while (true) {
+            audio = files[Math.floor(Math.random() * files.length)];
+            console.log('Searching .mp3 file...');
+            if (audio.endsWith('.mp3')) {
+                break;
+            }
+        }
+
+        dispatcher = connection.play('./music/' + audio);
+
+        dispatcher.on('start', () => {
+
+        });
+
+        dispatcher.on('error', console.error);
+
+        dispatcher.on('finish', () => {
+            voiceChannel = bot.channels.cache.get(config.voiceChannel);
+            dispatcher.destroy();
+        });
 
     });
-    
-    dispatcher.on('error', console.error);
-
-    dispatcher.on('finish', () => {
-      voiceChannel = bot.channels.cache.get(config.voiceChannel);
-      dispatcher.destroy();
-      voiceChannel.leave();
-    });
-    
-  }).catch(e => {
-    console.error(e);
-  });
-  
 }
 
 bot.on('ready', () => {
-  console.log('Bot is ready!');
-  console.log(`Logged in as ${bot.user.tag}!`);
-  console.log(`Prefix: ${config.prefix}`);
-  console.log(`Owner ID: ${config.botOwner}`);
-  console.log(`Voice Channel: ${config.voiceChannel}`);
-  console.log(`Status Channel: ${config.statusChannel}\n`);
+    console.log('Bot is ready!');
+    console.log(`Logged in as ${bot.user.tag}!`);
+    console.log(`Prefix: ${config.prefix}`);
+    console.log(`Owner ID: ${config.botOwner}`);
+    console.log(`Voice Channel: ${config.voiceChannel}`);
+    console.log(`Status Channel: ${config.statusChannel}\n`);
 
-  bot.user.setPresence({
-    activity: {
-      name: `SEVENTEEN (세븐틴) 'Left & Right'`,
-      type: 'LISTENING'
-    },
-    status: 'online',
-  }).then(presence => console.log(`Activity set to "${presence.activities[0].name}"`)).catch(console.error);
+    bot.user.setPresence({
+        activity: {
+            name: `SEVENTEEN (세븐틴) 'Left & Right'`,
+            type: 'LISTENING'
+        },
+        status: 'online',
+    }).then(presence => console.log(`Activity set to "${presence.activities[0].name}"`)).catch(console.error);
 
-  const readyEmbed = new Discord.MessageEmbed()
-  .setAuthor(`${bot.user.username}`, bot.user.avatarURL())
-  .setDescription('La madre negra de Eva ha despertado...')
-  .setColor('#0066ff')
+    const readyEmbed = new Discord.MessageEmbed()
+        .setAuthor(`${bot.user.username}`, bot.user.avatarURL())
+        .setDescription('La madre negra de Eva ha despertado...')
+        .setColor('#0066ff')
 
-  let statusChannel = bot.channels.cache.get(config.statusChannel);
-  if (!statusChannel) return console.error('The status channel does not exist! Skipping.');
-  statusChannel.send(readyEmbed);
-  console.log('Connected to the voice channel.');
-  //playAudio();
+    let statusChannel = bot.channels.cache.get(config.statusChannel);
+    if (!statusChannel) return console.error('The status channel does not exist! Skipping.');
+    statusChannel.send(readyEmbed);
+    console.log('Connected to the voice channel.');
+    //playAudio();
 });
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
-  let newUserChannel = newMember.channelID;
-  let oldUserChannel = oldMember.channelID;
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;
 
-  if(newUserChannel === "156502105379700740" && newMember.id === "146323942934708225") //don't remove ""
-  {
-    if(newMember.serverDeaf === oldMember.serverDeaf &&
-        newMember.serverMute === oldMember.serverMute &&
-        newMember.selfDeaf === oldMember.selfDeaf &&
-        newMember.selfMute === oldMember.selfMute &&
-        newMember.selfVideo === oldMember.selfVideo &&
-        newMember.streaming === oldMember.streaming
-    ){
-    playAudio();
+    if (newMember.id === "146323942934708225" || newMember.id === "180956256880689152") //don't remove ""
+    {
+        if (newUserChannel === "156502105379700740") {
+            if (newMember.serverDeaf === oldMember.serverDeaf &&
+                newMember.serverMute === oldMember.serverMute &&
+                newMember.selfDeaf === oldMember.selfDeaf &&
+                newMember.selfMute === oldMember.selfMute &&
+                newMember.selfVideo === oldMember.selfVideo &&
+                newMember.streaming === oldMember.streaming
+            ) {
+                playAudio();
+            }
+        }else{
+            sigueYEructa();
+        }
+    } else {
+        // User leaves a voice channel
     }
-  }
-  else{
-    // User leaves a voice channel
-  }
 });
 
 bot.on('message', async msg => {
-  if (msg.author.bot) return;
-  if (!msg.guild) return;
-  if (!msg.content.startsWith(config.prefix)) return;
-  let command = msg.content.split(' ')[0];
-  command = command.slice(config.prefix.length);
+    if (msg.author.bot) return;
+    if (!msg.guild) return;
+    if (!msg.content.startsWith(config.prefix)) return;
+    let command = msg.content.split(' ')[0];
+    command = command.slice(config.prefix.length);
 
-  // Public allowed commands
+    // Public allowed commands
 
-  if (command == '') {
-    playAudio();
-  }
+    if (command == '') {
+        playAudio();
+    }
 
-  if (![config.botOwner].includes(msg.author.id)) return;
+    if (![config.botOwner].includes(msg.author.id)) return;
 
-  // Bot owner exclusive
+    // Bot owner exclusive
 
-  if (command == 'join') {
-    msg.reply('Joining voice channel.');
-    console.log('Connected to the voice channel.');
-    playAudio();
-  }
+    if (command == 'join') {
+        msg.reply('Joining voice channel.');
+        console.log('Connected to the voice channel.');
+        playAudio();
+    }
 
-  if (command == 'resume') {
-    msg.reply('Resuming music.');
-    dispatcher.resume();
-  }
+    if (command == 'resume') {
+        msg.reply('Resuming music.');
+        dispatcher.resume();
+    }
 
-  if (command == 'pause') {
-    msg.reply('Pausing music.');
-    dispatcher.pause();
-  }
+    if (command == 'pause') {
+        msg.reply('Pausing music.');
+        dispatcher.pause();
+    }
 
-  if (command == 'skip') {
-    msg.reply('Skipping `' + audio + '`...');
-    dispatcher.pause();
-    dispatcher = null;
-    playAudio();
-  }
+    if (command == 'skip') {
+        msg.reply('Skipping `' + audio + '`...');
+        dispatcher.pause();
+        dispatcher = null;
+        playAudio();
+    }
 
-  if (command == 'leave') {
-    voiceChannel = bot.channels.cache.get(config.voiceChannel);
-    if (!voiceChannel) return console.error('The voice channel does not exist!\n(Have you looked at your configuration?)');
-    msg.reply('Leaving voice channel.');
-    console.log('Leaving voice channel.');
-    fileData = "Now Playing: Nothing";
-    fs.writeFile("now-playing.txt", fileData, (err) => { 
-    if (err) 
-    console.log(err); 
-    }); 
-    audio = "Not Playing";
-    dispatcher.destroy();
-    voiceChannel.leave();
-  }
+    if (command == 'leave') {
+        voiceChannel = bot.channels.cache.get(config.voiceChannel);
+        if (!voiceChannel) return console.error('The voice channel does not exist!\n(Have you looked at your configuration?)');
+        msg.reply('Ya me voy cagoento.');
+        console.log('Leaving voice channel.');
+        fileData = "Now Playing: Nothing";
+        fs.writeFile("now-playing.txt", fileData, (err) => {
+            if (err)
+                console.log(err);
+        });
+        audio = "Not Playing";
+        dispatcher.destroy();
+        voiceChannel.leave();
+    }
 
-  if (command == 'stop') {
-    await msg.reply('Ya me voy ya...');
-    fileData = "Now Playing: Nothing";
-    await fs.writeFile("now-playing.txt", fileData, (err) => { 
-    if (err) 
-    console.log(err); 
-    }); 
-    const statusEmbed = new Discord.MessageEmbed()
-    .setAuthor(`${bot.user.username}`, bot.user.avatarURL())
-    .setDescription(`${bot.user.username} se va A LA CAMA!`)
-    .setColor('#0066ff')
-    let statusChannel = bot.channels.cache.get(config.statusChannel);
-    if (!statusChannel) return console.error('The status channel does not exist! Skipping.');
-    await statusChannel.send(statusEmbed);
-    console.log('Ya me voy ya...');
-    dispatcher.destroy();
-    bot.destroy();
-    process.exit(0);
-  }
+    if (command == 'stop') {
+        await msg.reply('Ya me voy ya...');
+        fileData = "Now Playing: Nothing";
+        await fs.writeFile("now-playing.txt", fileData, (err) => {
+            if (err)
+                console.log(err);
+        });
+        const statusEmbed = new Discord.MessageEmbed()
+            .setAuthor(`${bot.user.username}`, bot.user.avatarURL())
+            .setDescription(`${bot.user.username} se va A LA CAMA!`)
+            .setColor('#0066ff')
+        let statusChannel = bot.channels.cache.get(config.statusChannel);
+        if (!statusChannel) return console.error('The status channel does not exist! Skipping.');
+        await statusChannel.send(statusEmbed);
+        console.log('Ya me voy ya...');
+        dispatcher.destroy();
+        bot.destroy();
+        process.exit(0);
+    }
 
 });
